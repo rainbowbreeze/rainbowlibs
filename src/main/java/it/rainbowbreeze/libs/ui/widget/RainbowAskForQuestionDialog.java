e0c0bb6bc8508c373032dab41f5c9fa6dcad2d39
rainbowbreeze <rainbowbreeze@gmx.com>
2015-05-17 01:34:43 +0200

    added compatibility options

--- src/main/java/it/rainbowbreeze/libs/ui/widget/RainbowAskForQuestionDialog.java
+++ src/main/java/it/rainbowbreeze/libs/ui/widget/RainbowAskForQuestionDialog.java
@@ -3,9 +3,9 @@ package it.rainbowbreeze.libs.ui.widget;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 
 /**
  * Created by alfredomorresi on 16/05/15.
